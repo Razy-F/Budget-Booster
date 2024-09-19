d2e0f075cfbff2e6818d3aacda1adaea6af9f20d
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-19 23:13:23 +0300

    - Added the credentials provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,23 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { userData } from "./data/users";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      authorize: async (credentials) => {
+        console.log("this are credentials ", credentials);
+        const user = userData.find(
+          (user) =>
+            user.username === credentials.username &&
+            user.password === credentials.password
+        );
+        if (user) {
+          return { ...user, name: user.username };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 });
