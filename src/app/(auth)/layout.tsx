0de4e0afc8a1e651b1bef27ae9d1b2b668b29bc5
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-22 22:14:26 +0300

    - Added new shadcn components - Build a new layout.tsx file that wraps the login and signin forms for authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex-center bg-primary-50 absolute left-0 top-0 z-[-1] min-h-screen w-full bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  );
+}
