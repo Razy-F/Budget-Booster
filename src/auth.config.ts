5e406b7ff09307465cb1e6e2e9d845c7fb4f75d7
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-23 01:07:25 +0300

    - Modified the auth.config.ts file to redirect users to the sign-in page when the authorized method returns false for unauthenticated users and returns true for authenticated users

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -7,22 +7,10 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isHomePage = nextUrl.pathname === "/";
-      if (isHomePage && !isLoggedIn) {
-        console.log("login first");
-        return false;
-      } else if (isLoggedIn) {
-        if (nextUrl.searchParams.get("callbackUrl")) {
-          console.log("render the callback");
-          return Response.redirect(
-            new URL(nextUrl.searchParams.get("callbackUrl") as string)
-          );
-        } else {
-          return true;
-        }
-      } else {
-        false;
-      }
+
+      if (isLoggedIn) {
+        return true;
+      } else return false;
     },
   },
   providers: [], // Add providers with an empty array for now
