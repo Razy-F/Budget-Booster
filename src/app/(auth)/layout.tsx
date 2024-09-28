19fdbdb44dc47df21335caa4dcc25bff7cf47273
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-29 00:32:32 +0300

    - Added the logo component to the layout of the auth wrapper

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+import Logo from "@/components/Logo";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex-center bg-primary-50 absolute left-0 top-0 z-[-1] min-h-screen w-full bg-dotted-pattern bg-cover bg-fixed bg-center">
+    <div className="flex-center bg-no-repeat flex-col bg-primary-50  min-h-screen w-full bg-dotted-pattern bg-cover bg-fixed bg-center">
+      <Logo />
       {children}
     </div>
   );
