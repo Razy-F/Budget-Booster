42e81f04d6d5e6097359ef391db828ae2319f14b
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-30 01:16:19 +0300

    - Change the color of the theme Include the dark/light mode support and remove the hydration warning fron the HTML tag

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from "@/auth";
 export const config = {
   //this regex avoids running the middleware on paths such as the favicon or static images
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png).*)"],
 };
