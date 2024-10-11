73f7feab4d3451001678ef39d1aa59d35e485387
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-10-12 00:08:23 +0300

    - Create a the LogoMobile component that is optimized for small screen devices

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -14,4 +14,16 @@ const Logo = () => {
   );
 };
 
+export const LogoMobile = () => {
+  return (
+    <div>
+      <Link href="/" className="flex items-center gap-2" prefetch={false}>
+        <p className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-2xl font-bold leading-tight tracking-tighter text-transparent">
+          BudgetTracker
+        </p>
+      </Link>
+    </div>
+  );
+};
+
 export default Logo;
