ff468e6807c48584a614883fc96de301a6f27ba9
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-10-22 22:10:39 +0300

    - Modified the auth.config file

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -5,12 +5,24 @@ export const authConfig = {
     signIn: "/log-in",
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth }) {
       const isLoggedIn = !!auth?.user;
 
       if (isLoggedIn) {
         return true;
       } else return false;
+    },
+    async session({ session, token }) {
+      // Add the user id to the session object
+      if (token.sub) session.user.id = token.sub;
+      return session;
+    },
+    async jwt({ token, user }) {
+      // This is called when a user signs in
+      if (user) {
+        token.id = user.id; // Add the user id to the token
+      }
+      return token;
     },
   },
   providers: [], // Add providers with an empty array for now
