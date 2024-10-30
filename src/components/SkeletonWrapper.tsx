0752fcf2a248c9cafe89fd16376c7a4c23899e5e
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-10-31 00:14:32 +0200

    - included the skeleton component from the Shadcn library and integrated it into the SkeletonWrapper component

---
+++ src/components/SkeletonWrapper.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Skeleton } from "./ui/skeleton";
+import { cn } from "@/lib/utils";
+type Props = {
+  children: React.ReactNode;
+  isLoading: boolean;
+  fullWidth?: boolean;
+};
+
+const SkeletonWrapper = ({ children, isLoading, fullWidth = true }: Props) => {
+  if (!isLoading) return children;
+  return (
+    <Skeleton className={cn(fullWidth && "w-full")}>
+      <div className="opacity-0">{children}</div>
+    </Skeleton>
+  );
+};
+
+export default SkeletonWrapper;
