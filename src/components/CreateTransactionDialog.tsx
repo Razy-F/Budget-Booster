a46b77c7b0383a7282a84a02c9f8413c935a48e0
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-11-07 23:25:30 +0200

    - Incorporated the dialog component into the createTransaction component

--- src/components/CreateTransactionDialog.tsx
+++ src/components/CreateTransactionDialog.tsx
@@ -1,11 +1,39 @@
 "use client";
 import React from "react";
 import { TransactionType } from "@/lib/types";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 type Props = {
   children: React.ReactNode;
   type: TransactionType;
 };
 const CreateTransactionDialog = ({ children, type }: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create a new
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-rose-500"
+              )}
+            >
+              {type}
+            </span>
+            transaction
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CreateTransactionDialog;
