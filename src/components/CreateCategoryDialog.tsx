324c0fe87a6b81c390a55e6ad7e7ca9de608e8f2
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-11-11 21:07:24 +0200

    - Incorporated the dialog component into the CreateCategoryDialog component

--- src/components/CreateCategoryDialog.tsx
+++ src/components/CreateCategoryDialog.tsx
@@ -1,5 +1,60 @@
 "use client";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { CircleOff, Loader2, PlusSquare } from "lucide-react";
 const CreateCategoryDialog = ({ type }: Props) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex border-separate items-center justify-start rounded-none border-b p-3 text-muted-foreground"
+        >
+          <PlusSquare className="mr-2 size-4" />
+          Create new
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-rose-500"
+              )}
+            >
+              {type}
+            </span>
+            category
+          </DialogTitle>
+          <DialogDescription>
+            Categories are used to group your transactions
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={"destructive"} onClick={() => form.reset()}>
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>
+            {!isPending && "Create"}
+            {isPending && <Loader2 className="animate-spin" />}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CreateCategoryDialog;
