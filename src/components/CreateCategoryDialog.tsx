a5ed089100fea6965c2a5fe00187d25d48490274
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-11-12 23:49:52 +0200

    - Set up and define the form element within the CreateCategory dialog

--- src/components/CreateCategoryDialog.tsx
+++ src/components/CreateCategoryDialog.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useForm } from "react-hook-form";
 import {
   Dialog,
   DialogClose,
@@ -11,6 +12,19 @@ import {
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { CircleOff, Loader2, PlusSquare } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
 const CreateCategoryDialog = ({ type }: Props) => {
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -41,6 +55,75 @@ const CreateCategoryDialog = ({ type }: Props) => {
             Categories are used to group your transactions
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction amount{" "}
+                    <span className="text-destructive font-extrabold">*</span>
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="icon"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Icon</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className="h-[100px] w-full"
+                        >
+                          {form.watch("icon") ? (
+                            <div className="flex flex-col items-center gap-2">
+                              <span className="text-5xl" role="img">
+                                {field.value}
+                              </span>
+                              <p className="text-xs text-muted-foreground">
+                                Click to change
+                              </p>
+                            </div>
+                          ) : (
+                            <div className="flex flex-col items-center gap-2">
+                              <CircleOff className="size-[48px]" />
+                              <p className="text-xs text-muted-foreground">
+                                Click to select
+                              </p>
+                            </div>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full">
+                        <Picker
+                          data={data}
+                          onEmojiSelect={(emoji: { native: string }) =>
+                            field.onChange(emoji.native)
+                          }
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormDescription>
+                    This is how your category will appear in the application{" "}
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+
         <DialogFooter>
           <DialogClose asChild>
             <Button variant={"destructive"} onClick={() => form.reset()}>
