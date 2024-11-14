283d065fb54266b386f25bcaf01d7a89df924384
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-11-15 01:02:05 +0200

    Developing the CategoryPicker component - Building the popover and handiling the selected category

--- src/components/CategoryPicker.tsx
+++ src/components/CategoryPicker.tsx
@@ -1,9 +1,42 @@
 "use client ";
 import { TransactionType } from "@/lib/types";
+import { Category } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
 type Props = {
   type: TransactionType;
 };
 const CategoryPicker = ({ type }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () =>
+      fetch("/api/categories?type" + type).then((res) => res.json()),
+  });
+
+  const selectedCategory = categoriesQuery.data?.find(
+    (category: Category) => category.name === value
+  );
+  return (
+    <Popover modal={true} open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          role="combobox"
+          aria-expanded={isOpen}
+          className="w-[200px] justify-between"
+        >
+          {selectedCategory ? (
+            <CategoryRow category={selectedCategory} />
+          ) : (
+            "Selected category"
+          )}
+          <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  );
 };
 
 export default CategoryPicker;
