fc1a28ec47e8756f605bcca325cee27eb0f55ba4
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-11-22 22:07:44 +0200

    Developing the CategoryPicker component - Developed the CategoryRow component that displays a single category

--- src/components/CategoryPicker.tsx
+++ src/components/CategoryPicker.tsx
@@ -40,3 +40,12 @@ const CategoryPicker = ({ type }: Props) => {
 };
 
 export default CategoryPicker;
+
+function CategoryRow({ category }: { category: Category }) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role="img">{category.icon}</span>
+      <span>{category.name}</span>
+    </div>
+  );
+}
