59cd6fe733eab3e1852fa1bfe536fa5d1cfb6f3f
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-12-03 23:06:27 +0200

    Complete the development of the CreateCategory server action

--- src/lib/server/actions/categories.ts
+++ src/lib/server/actions/categories.ts
@@ -1,10 +1,32 @@
+"use server";
+
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
 import {
   CreateCategorySchema,
   CreateCategorySchemaType,
 } from "@/lib/zod/schema/categories";
+import { redirect } from "next/navigation";
+
 export async function CreateCategory(form: CreateCategorySchemaType) {
   const parsedBody = CreateCategorySchema.safeParse(form);
   if (!parsedBody.success) {
     throw new Error("bad request");
   }
+
+  const user = await auth();
+  if (!user || !user.user || !user.user.id) {
+    redirect("/log-in");
+  }
+
+  const { name, icon, type } = parsedBody.data;
+
+  return await prisma.category.create({
+    data: {
+      userId: user.user.id,
+      name,
+      type,
+      icon,
+    },
+  });
 }
