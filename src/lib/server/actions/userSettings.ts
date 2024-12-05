43611a8321aad787a20cfece5ca8696e8c508bd0
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-12-05 16:15:51 +0200

    Resume the development of the CreateCategory server action

--- src/lib/server/actions/userSettings.ts
+++ src/lib/server/actions/userSettings.ts
@@ -1,5 +1,11 @@
 "use server";
+
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import { UpdateUserCurrencySchema } from "@/lib/zod/schema/userSettings";
 import { Currencies } from "@prisma/client";
+import { redirect } from "next/navigation";
+
 export async function UpdateUserCurrency(currency: Currencies) {
   const parsedBody = UpdateUserCurrencySchema.safeParse({
     currency,
@@ -8,4 +14,19 @@ export async function UpdateUserCurrency(currency: Currencies) {
   if (!parsedBody.success) {
     throw parsedBody.error;
   }
+
+  const user = await auth();
+  if (!user || !user.user || !user.user.id) {
+    redirect("/log-in");
+  }
+  const userSettings = await prisma.userSettings.update({
+    where: {
+      userId: user.user.id,
+    },
+    data: {
+      currency,
+    },
+  });
+
+  return userSettings;
 }
