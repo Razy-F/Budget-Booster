629eae05d750a70aba6409d0a5b17171613dc612
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-11-19 23:40:21 +0200

    Improve the manner of responsiveness

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,14 +6,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex-center flex-col bg-primary-50  min-h-screen w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+    <div className="flex-center flex-col bg-primary-50  min-h-screen w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] relative">
       <Logo />
       <div
-        className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl"
+        className="absolute top-0 -z-10  blur-3xl inset-0"
         aria-hidden="true"
       >
         <div
-          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-primary to-secondary opacity-30"
+          className="aspect-[1155/678]  bg-gradient-to-tr from-primary to-secondary opacity-30"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
