448f6511f7b902b4a2b6d7bfc161f5875408995c
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-12-18 00:09:48 +0200

    Began working on the CategoriesStats + StatsCards components

--- src/components/CategoriesStats.tsx
+++ src/components/CategoriesStats.tsx
@@ -1,3 +1,4 @@
+import { UserSettings } from "@prisma/client";
 type Props = {
   userSettings: UserSettings;
   from: Date;
@@ -5,4 +6,27 @@ type Props = {
 };
 
 function CategoriesStats({ userSettings, from, to }: Props) {
+  return (
+    <div className="flex w-full flex-wrap gap-2 md:flex-nowrap">
+    </div>
+  );
+}
+
+export default CategoriesStats;
+
+type CategoriesCardPropsType = {
+  formatter: Intl.NumberFormat;
+  type: "income" | "expense";
+  data: getCategoriesActionDataType;
+};
+function CategoriesCard({ formatter, type, data }: CategoriesCardPropsType) {
+  return (
+    <Card className="h-80 w-full col-span-6">
+      <CardHeader>
+        <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col">
+          {type === "income" ? "Incomes" : "Expenses"} by category
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  );
 }
--- src/components/StatsCards.tsx
+++ src/components/StatsCards.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 
 type Props = {
   userSettings: UserSettings;
@@ -7,4 +8,24 @@ type Props = {
 };
 
 function StatsCards({ userSettings, from, to }: Props) {
+  return (
+    <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
+    </div>
+  );
+}
+
+export default StatsCards;
+
+type StatsCardProps = {
+  formatter: Intl.NumberFormat;
+  value: number;
+  title: string;
+  icon: ReactNode;
+};
+function StatsCard({ formatter, value, title, icon }: StatsCardProps) {
+  return (
+    <Card className="flex h-24 w-full items-center gap-2 p-4">
+      {icon}
+    </Card>
+  );
 }
