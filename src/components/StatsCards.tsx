f93a2f4ee9f62df5253a25eb977a896673b41205
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-12-23 21:20:26 +0200

    Continuing to work on the Overview component

--- src/components/StatsCards.tsx
+++ src/components/StatsCards.tsx
@@ -1,5 +1,10 @@
 "use client";
+import { getBalanceAction } from "@/lib/server/actions/balance";
+import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "./SkeletonWrapper";
+import { Card } from "./ui/card";
+import { useCurrencyFormatter } from "@/hooks/use-currency-formatter";
 
 type Props = {
   userSettings: UserSettings;
@@ -8,8 +13,30 @@ type Props = {
 };
 
 function StatsCards({ userSettings, from, to }: Props) {
+  const statsQuery = useQuery({
+    queryKey: ["overview", "stats", from, to],
+    queryFn: () =>
+      getBalanceAction({ from: dateToUTCDate(from), to: dateToUTCDate(to) }),
+  });
+  const formatter = useCurrencyFormatter(userSettings.currency);
+
+  const income = statsQuery.data?.income || 0;
+  const expense = statsQuery.data?.expense || 0;
+
+  const balance = income - expense;
+
   return (
     <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <StatsCard
+          formatter={formatter}
+          value={income}
+          title="income"
+          icon={
+            <TrendingUp className="size-12 items-center rounded-lg p-2 text-emerald-500 bg-emerald-400/10" />
+          }
+        />
+      </SkeletonWrapper>
     </div>
   );
 }
@@ -26,6 +53,9 @@ function StatsCard({ formatter, value, title, icon }: StatsCardProps) {
   return (
     <Card className="flex h-24 w-full items-center gap-2 p-4">
       {icon}
+      <div className="flex flex-col items-start gap-0">
+        <p className="text-muted-foreground">{title}</p>
+      </div>
     </Card>
   );
 }
