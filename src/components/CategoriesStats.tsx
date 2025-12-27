5c0ea8dfbf357df4ac513b3d2a9d829c5dd648e5
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-12-27 22:29:56 +0200

    Continuing to work on the CategoriesStats component

--- src/components/CategoriesStats.tsx
+++ src/components/CategoriesStats.tsx
@@ -1,5 +1,16 @@
+"use client";
+import { useCurrencyFormatter } from "@/hooks/use-currency-formatter";
+import {
+  getCategoriesAction,
+  getCategoriesActionDataType,
+} from "@/lib/server/actions/categories";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "./SkeletonWrapper";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Progress } from "./ui/progress";
+import { ScrollArea } from "./ui/scroll-area";
+
 type Props = {
   userSettings: UserSettings;
   from: Date;
@@ -53,6 +64,37 @@ function CategoriesCard({ formatter, type, data }: CategoriesCardPropsType) {
               {type === "expense" ? "expense" : "income"}
             </p>
           </div>
+        )}
+        {filterData.length > 0 && (
+          <ScrollArea className="h-60 w-full px-4">
+            <div className="flex w-full flex-col gap-4 p-4">
+              {filterData.map((item) => {
+                const amount = item._sum.amount || 0;
+                const percentage = (amount * 100) / (total || amount);
+                return (
+                  <div className="flex flex-col gap-2" key={item.category}>
+                    <div className="flex items-center justify-between">
+                      <span className="flex items-center text-gray-400">
+                        {item.categoryIcon} {item.category}
+                        <span className="ml-2 text-xs text-muted-foreground">
+                          ({percentage.toFixed(0)}%)
+                        </span>
+                      </span>
+                      <span className="text-sm text-gray-400">
+                        {formatter.format(amount)}
+                      </span>
+                    </div>
+                    <Progress
+                      value={percentage}
+                      indicator={
+                        type === "expense" ? "bg-red-500" : "bg-emerald-500"
+                      }
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </ScrollArea>
         )}
       </div>
     </Card>
