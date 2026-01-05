497d775026864721782d2db4a87d0e1e200e18bb
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-01-05 23:58:17 +0200

    Continue to work on the HistoryPeriodSelector component

--- src/components/HistoryPeriodSelector.tsx
+++ src/components/HistoryPeriodSelector.tsx
@@ -1,4 +1,7 @@
 import { Period, Timeframe } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "./SkeletonWrapper";
+import { GetHistoryPeriodResType } from "@/app/api/history-periods/route";
 function HistoryPeriodSelector({
   period,
   setPeriod,
@@ -10,8 +13,19 @@ function HistoryPeriodSelector({
   timeframe: Timeframe;
   setTimeframe: (timeframe: Timeframe) => void;
 }) {
+  const historyPeriods = useQuery<GetHistoryPeriodResType>({
+    queryKey: ["overview", "history", "periods"],
+    queryFn: () =>
+      fetch("/api/history-periods", {
+        next: {
+          revalidate: 60,
+        },
+      }).then((res) => res.json()),
+  });
   return (
     <div className="flex flex-wrap items-center gap-4">
+      <SkeletonWrapper isLoading={historyPeriods.isFetching} fullWidth={false}>
+      </SkeletonWrapper>
     </div>
   );
 }
