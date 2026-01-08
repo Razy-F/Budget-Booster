9ed7100ec7c52c8ccb797c566f732866489a5866
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-01-09 00:02:51 +0200

    Complete the development of the HistoryPeriodSelector component

--- src/components/HistoryPeriodSelector.tsx
+++ src/components/HistoryPeriodSelector.tsx
@@ -78,6 +78,32 @@ function MonthSelector({
   setPeriod: (period: Period) => void;
 }) {
   return (
+    <Select
+      value={period.month.toString()}
+      onValueChange={(value) => {
+        setPeriod({
+          year: period.year,
+          month: parseInt(value),
+        });
+      }}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a year" />
+      </SelectTrigger>
+      <SelectContent>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((month) => {
+          const monthStr = new Date(period.year, month, 1).toLocaleString(
+            "default",
+            { month: "long" }
+          );
+          return (
+            <SelectItem key={month} value={month.toString()}>
+              {monthStr}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
   );
 }
 function YearSelector({
