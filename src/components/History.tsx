d7e990f9daf4eb9627ecc3ce39be97c5fe2d9fc4
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-01-12 22:01:49 +0200

    Continue to work on the History component

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -58,6 +58,20 @@ function CustomTooltip({ active, payload, formatter }: any) {
         bgColor="bg-red-500"
         textColor="text-red-500"
       />
+      <TooltipRow
+        formatter={formatter}
+        label="Income"
+        value={income}
+        bgColor="bg-emerald-500"
+        textColor="text-emerald-500"
+      />
+      <TooltipRow
+        formatter={formatter}
+        label="Balance"
+        value={income - expense}
+        bgColor="bg-gray-100"
+        textColor="text-foreground"
+      />
     </div>
   );
 }
