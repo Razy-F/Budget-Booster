4ad96ab8af2cb236a50698dc6091de96b18a1066
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-01-28 00:46:37 +0200

    Developing the transaction Table

---
+++ src/components/TransactionTable.tsx
@@ -0,0 +1,14 @@
+function TransactionTable({ from, to }: { from: Date; to: Date }) {
+  return (
+    <div className="w-full">
+      <div className="flex flex-wrap items-end justify-between gap-2 py-4">
+        <div className="flex gap-2">
+      </div>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+        </div>
+    </div>
+  );
+}
+
+export default TransactionTable;
