eecbad3cfc78144a24dfbede6c4cf93886c267c0
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-01-29 22:20:06 +0200

    Continue to work on the Transaction table component

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -1,3 +1,26 @@
+import { GetTransactionHistoryResType } from "@/app/api/transaction-history/route";
+export type TransactionHistoryRow = GetTransactionHistoryResType[0];
+const columns: ColumnDef<TransactionHistoryRow>[] = [
+  {
+    accessorKey: "category",
+    header({ column }) {
+      return <DataTableColumnHeader column={column} title="Category" />;
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+    cell({ row }) {
+      return (
+        <div className="flex gap-2 capitalize">
+          {row.original.categoryIcon}
+          <div className="capitalize">{row.original.category}</div>
+        </div>
+      );
+    },
+  },
+];
+
+
 function TransactionTable({ from, to }: { from: Date; to: Date }) {
   return (
     <div className="w-full">
