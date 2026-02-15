17235456b97bb75dd530956b3dcf511ea1004f9f
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-02-15 15:32:05 +0200

    Continue to work on the Transaction table component

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -134,6 +134,34 @@ function TransactionTable({ from, to }: { from: Date; to: Date }) {
                 </TableRow>
               ))}
             </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
           </Table>
         </div>
         <div className="flex items-center justify-end space-x-2 py-4">
